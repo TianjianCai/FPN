90c1f9d265386bbaa6365b1dda6a1863e7f48bfb
Tianjian Cai <tjcaiforapp@gmail.com>
2018-07-19 13:33:59 +0800

    update
    remove the wrong inception model

--- fpn.py
+++ fpn.py
@@ -5,30 +5,32 @@ import matplotlib.pyplot as plt
 
 SAVE_SESS = True;
 
-class conv3x3(object):
-    def __init__(self,x,stride=1,c1=None,c2=None):
-        w1 = tf.Variable(tf.random_normal(shape=[1,3,c1,c1],stddev=0.1));
-        w2 = tf.Variable(tf.random_normal(shape=[3,1,c1,c2],stddev=0.1));
+class conv7x7(object):
+    def __init__(self,x,stride=1,c1=None,c2=None,BN=True):
+        w = tf.Variable(tf.random_normal(shape=[7,7,c1,c2],stddev=1));
         b = tf.Variable(tf.zeros(c2));
-        beta = tf.Variable(tf.zeros(c2));
-        gamma = tf.Variable(tf.ones(c2));
-        mid = tf.nn.conv2d(input = x, filter = w1, strides = [1,1,stride,1], padding = 'VALID');
-        fin = tf.nn.conv2d(input = mid, filter = w2, strides = [1,stride,1,1], padding = 'VALID');
-        bef_bn = tf.nn.bias_add(fin, b);
+        conv_out = tf.nn.conv2d(input = x, filter = w, strides = [1,stride,stride,1], padding = 'VALID');
+        net_out = tf.nn.bias_add(conv_out, b);
+        
+        beta = tf.Variable(0.5*tf.ones(c2));
+        gamma = tf.Variable(0.25*tf.ones(c2));
         aft_bn,_,_ = tf.nn.fused_batch_norm(
-            x=bef_bn, 
+            x=net_out, 
             scale=gamma, 
             offset=beta, 
             is_training=True);
-        self.out = aft_bn;
+        if BN:
+            self.out = tf.clip_by_value(aft_bn, 0, 1);
+        else:
+            self.out = aft_bn;
+
 
 files = ['res/' + s for s in sorted(os.listdir('res/'))];
 img_raw = tf.stack([tf.read_file(file) for file in files]);
 img_decoded = tf.cast(tf.map_fn(fn = tf.image.decode_jpeg, elems = img_raw, dtype=tf.uint8),tf.float32)/256;
 
-layer1 = conv3x3(img_decoded,1,3,256);
-layer2 = conv3x3(layer1.out,1,256,256);
-layer3 = conv3x3(layer2.out,2,256,3);
+layer1 = conv7x7(img_decoded,2,3,256,False);
+layer2 = conv7x7(layer1.out,2,256,3,False);
 
 sess = tf.Session();
 sess.run(tf.global_variables_initializer());
@@ -43,8 +45,7 @@ if SAVE_SESS is True:
     
     saver.save(sess,os.getcwd()+'/save/save.ckpt');
 
-
-convout = np.array(sess.run(layer3.out));
+convout = np.array(sess.run(layer2.out));
 convin = np.array(sess.run(img_decoded));
 print(convout);
 print(np.shape(convin));
